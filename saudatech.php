eb486f22ffde3de1d3cbd8b7967d46b6f55195d3
mukti <46982965+muktikalsekar@users.noreply.github.com>
2020-08-02 12:05:52 +0530

    Add files via upload

---
+++ saudatech.php
@@ -0,0 +1,26 @@
+<html>
+<head>
+<title>SaudaTech</title>
+<body>
+<h1 style="text-align:center"><font face="times new roman" size="+10" >Sauda Tech</font></h1>
+</body>
+
+<link rel="stylesheet" type="text/css" href="style.css">
+<div class="loginbox">
+<form action="task.php" method="post" align="center">
+		<p>Username</p>
+		<input type="text" name="Username" placeholder="Enter Username" required>
+		
+		<p>Password</p>
+		<input type="Password" name="Password" placeholder="Enter Password" required>
+		<br>
+		<br>
+		
+		<input type="submit" name="submit" value="Login">
+      </form>
+</div>
+
+
+</head>
+</html>
+
